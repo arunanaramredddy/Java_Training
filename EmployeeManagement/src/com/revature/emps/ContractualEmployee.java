85f82d2c9d3855422b5b4459ccc905c60af2f141
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-03-08 23:07:41 +0530

    Day-04_Assignment

--- EmployeeManagement/src/com/revature/emps/ContractualEmployee.java
+++
@@ -1,5 +0,0 @@
-package com.revature.emps;
-
-public class ContractualEmployee {
-
-}
--- EmployeeManagement/src/com/revature/emps/EmployeeManager.java
+++
@@ -1,8 +0,0 @@
-package com.revature.emps;
-
-public class EmployeeManager {
-    public static void main(String[] args) {
-        //Employee emp=new Employee(101,"Aruna",10,200);
-        SalariedEmployee se
-    }
-}
