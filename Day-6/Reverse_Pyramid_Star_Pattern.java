99cb93ab770d7b93f79404bdab463296b1f5dbb7
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2024-03-13 22:56:36 +0530

    Assignment

---
+++ Day-6/Reverse_Pyramid_Star_Pattern.java
@@ -0,0 +1,25 @@
+/*
+
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+*/
+
+import java.util.Scanner;
+class Reverse_Pyramid_Star_Pattern{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(i-1)+1;j++)
+                System.out.print(" ");
+            for(int k=1;k<=(n-i)+1;k++)
+                System.out.print("* ");
+            System.out.println();
+
+        }
+    }
+}
---
+++ Demo/src/com/bank/Pattern.java
@@ -0,0 +1,29 @@
+package com.bank;
+
+
+import java.util.Scanner;
+public class Pattern {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter  the number of rows:");
+        int rows = sc.nextInt();
+        for(int i=1;i<=rows-1;i++) {
+            for(int j=1;j<=i;j++) {
+                if(j==1 || i==j) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+
+        }
+        for(int i=1;i<=rows;i++) {
+            System.out.print("*");
+        }
+
+    }
+
+}
